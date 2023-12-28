ec1fa6ff75385f123d81b50b94f695da0a7254ff
Rahim00001 <rahimlpt2022@gmail.com>
2023-12-28 23:23:26 +0600

    update and delete functionality added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,10 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-// const uri = "mongodb+srv://scicJobTask:3iTcZO3pa959gMRD@cluster0.lk92epi.mongodb.net/?retryWrites=true&w=majority";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lk92epi.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +24,58 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const taskCollection = client.db('jobTaskDB').collection('task')
+
+
+        app.get('/tasks', async (req, res) => {
+            const cursor = taskCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/tasks', async (req, res) => {
+            const newTask = req.body;
+            console.log(newTask);
+            const result = await taskCollection.insertOne(newTask);
+            res.send(result);
+        })
+
+        app.put('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedTask = req.body
+            const task = {
+                $set: {
+                    name: updatedTask.name,
+                    description: updatedTask.description,
+                    category: updatedTask.category,
+                }
+            }
+            const result = await taskCollection.updateOne(filter, task, options)
+            res.send(result)
+        })
+
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
